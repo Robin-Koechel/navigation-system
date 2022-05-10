0d6912f998103af48e365bd69e87d26524189f2e
hah23 <Josua.eyl@gmail.com>
2022-05-10 13:58:55 +0100

    Häuser könenn jetz auch geholt werden

--- code/parse_osm_files.py
+++ code/parse_osm_files.py
@@ -22,6 +22,51 @@ class osm_parser():
                 return lat, lon
         raise ValueError('id ist nicht vergeben oder ist falsch')
 
+    def get_Building_dicts(self):
+        st_Building_dicts = []
+
+        ways = self.bs_data.find_all('way')
+        lst_Building_tags = []
+        for way in ways:
+            tags_in_way = way.find_all('tag')
+            for tag in tags_in_way:
+                if tag.get('k') == "building":
+                    lst_Building_tags.append(way)
+        for building in lst_Building_tags:
+            dict = {}
+
+            id = building.get('id')
+            dict['id'] = id
+
+            lst_refs = []
+            nds = building.find_all('nd')
+            for nd in nds:
+                ref = nd.get('ref')
+                lst_refs.append(ref)
+            dict["lst_references"] = lst_refs
+
+            tags = building.find_all('tag')
+            for tag in tags:
+                key = tag.get('k')
+                value = tag.get('v')
+                dict[key] = value
+            st_Building_dicts.append(dict)
+
+        return st_Building_dicts
+
+    def get_Building_nodes(self, building_id):
+        lst_node_ids = []
+
+        res = []
+        for h in self.get_highway_dicts():
+            if h['id'] == str(building_id):
+                lst_node_ids = h['lst_references']
+                break
+        for node_id in lst_node_ids:
+            lat, lon = self.get_node_lat_lon(node_id)
+            res.append((lat, lon))
+        return res
+
     def get_highway_dicts(self):
         lst_highway_dicts = []
 
@@ -70,3 +115,5 @@ class osm_parser():
 
 p = osm_parser('data_gerlingen.osm')
 print(p.get_highway_nodes(24538510))
+dic = p.get_highway_dicts()
+print(p.get_highway_dicts())
