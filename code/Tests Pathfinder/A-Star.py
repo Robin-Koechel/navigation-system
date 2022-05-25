2ede23093310101dd72de29672bd5cecdab13b1f
Robin <robin.koechel@its-stuttgart.de>
2022-05-25 17:14:28 +0200

    habe A* jetzt soweit verstanden und kann ihn morgen dann für die unser Projekt implementieren

--- code/Tests Pathfinder/A-Star.py
+++ code/Tests Pathfinder/A-Star.py
@@ -14,8 +14,8 @@ class node():
         self.pointer = [x, y]
 
     def calc_f(self):
-        g = math.sqrt((pos_goal[0]-self.pos[0])**2 + (pos_goal[1]-self.pos[1])**2)
-        h = math.sqrt((pos_start[0] - self.pos[0]) ** 2 + (pos_start[1] - self.pos[1]) ** 2)
+        h = math.sqrt((pos_goal[0]-self.pos[0])**2 + (pos_goal[1]-self.pos[1])**2)
+        g = math.sqrt((pos_start[0] - self.pos[0]) ** 2 + (pos_start[1] - self.pos[1]) ** 2)
 
         return g+h
 
@@ -39,14 +39,45 @@ def get_min_f_open_lst():
     return min
 def get_successors(x, y):
     lst_successors = []
-def expand_Node():
-    pass
+    lst_successors.append(node(x-1,y-1))
+    lst_successors.append(node(x - 1, y))
+    lst_successors.append(node(x - 1, y+1))
+
+    lst_successors.append(node(x + 1, y - 1))
+    lst_successors.append(node(x + 1, y))
+    lst_successors.append(node(x + 1, y + 1))
+
+    lst_successors.append(node(x, y - 1))
+    lst_successors.append(node(x, y + 1))
+    return lst_successors
+
+def expand_Node(node):
+    successors_node = get_successors(node.pos[0], node.pos[1])
+
+    for successor in successors_node:
+        in_closed_lst = False
+        for i in closed_lst:
+            if i == successor:
+                in_closed_lst = True
+                break
+        if in_closed_lst:
+            continue
+        tentative_g = math.sqrt((pos_start[0] - successor.pos[0]) ** 2 + (pos_start[1] - successor.pos[1]) ** 2)
+
+        for i in open_lst:
+            if i == successor:
+                if tentative_g < i.calc_f():
+                    i.set_pointer(node.pos[0], node.pos[1])
+            else:
+                successor.set_pointer(node.pos[0], node.pos[1])
+
+
 
 open_lst.append(lst_nodes[pos_start[0]][pos_start[1]])
 
 while len(open_lst) > 0:
 
-    current_Node= get_min_f_open_lst()
+    current_Node = get_min_f_open_lst()
     open_lst.remove(current_Node)
 
     if current_Node.pos == pos_goal:
@@ -55,5 +86,6 @@ while len(open_lst) > 0:
 
     closed_lst.append(current_Node)
     expand_Node(current_Node)
+
 if len(open_lst) < 0:
     print("Path not found")
